9d922b66167f2b02f0bd58b2eb638f1cd3f3999b
Dan Burzo <danburzo@gmail.com>
2013-10-15 15:25:49 +0300

    Written actual css parser

--- gmaps-css.js
+++ gmaps-css.js
@@ -1,6 +1,29 @@
 var fs = require('fs');
 var parse = require('css-parse');
 
-var file = fs.readFileSync('test.css', 'utf-8');
-var css = parse(file);
-console.log(JSON.stringify(css, null, 2));
+var css = fs.readFileSync('test.css', 'utf-8');
+var json = parse(css);
+
+var styles = [];
+json.stylesheet.rules.forEach(function(rule) {
+	var selectors = rule.selectors.map(function(selector) {
+		var parts = selector.split(':');
+		return {
+			elementType: parts[0] || 'all',
+			featureType: parts[1] || 'all'
+		};
+	});
+
+	var stylers = rule.declarations.map(function(declaration) {
+		var styler = {};
+		styler[declaration.property] = declaration.value;
+		return styler;
+	});
+
+	selectors.forEach(function(selector) {
+		selector.stylers = stylers;
+		styles.push(selector);
+	});
+});
+
+console.log(JSON.stringify(styles, null, 2));
